6610324633eb6f84c0e72dd16205a8a274736c56
zyqzoey1122-tech <zyqzoey1122@gmail.com>
2026-01-15 15:04:38 +0800

    index file

---
+++ sw.js
@@ -0,0 +1,26 @@
+
+const CACHE_NAME = 'word-kingdom-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/index.tsx',
+  '/manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
